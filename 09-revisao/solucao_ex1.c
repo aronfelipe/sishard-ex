504e377c6a0f9f122bec04a13e86c75e24baca42
felipe <aronakamoto@outlook.com>
2020-03-30 15:12:25 -0300

    Feat: 09 revision ex 1

--- 09-revisao/solucao_ex1.c
+++ 09-revisao/solucao_ex1.c
@@ -1,2 +1,10 @@
 /* Implementar função solucao_ex1 */
 
+int solucao_ex1(long rdi, long rsi, long rdx, long rcx) {
+   long rax =  rdi + (rsi*1);
+   rcx = rax + (rdx*4);
+   rdi *= rdi;
+   rax = rdi + (rsi*2);
+   rdx += rax;
+   return rcx >= rdx;
+}
